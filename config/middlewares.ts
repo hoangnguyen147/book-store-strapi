62dcee2c7ac7a341c03f908342b9448aafe3272a
hoangnguyen147 <huyhoangnguyen147z@gmail.com>
2025-06-11 16:13:23 +0700

    [nokey] update cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      headers: '*',
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
